12a214e8d1043586457d1449e1510730394a4eda
sanjit <sanjit.est14@gmail.com>
2026-05-12 19:58:14 +0600

    Project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <div>Home</div>
+      <Button>Sign In</Button>
+    </div>
+  );
+}
