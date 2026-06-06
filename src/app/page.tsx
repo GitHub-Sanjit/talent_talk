59de3f0cc480ba8c033fbdc6455524d9ebb00b8c
sanjit <sanjit.est14@gmail.com>
2026-06-06 20:54:18 +0600

    Authentication Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello there!</h1>
-     <Button>Hi there</Button>
-    </div>
+    <header className="flex justify-end items-center p-4 gap-4 h-16">
+      <Show when="signed-out">
+        <SignInButton />
+        <SignUpButton>
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        </SignUpButton>
+      </Show>
+      <Show when="signed-in">
+        <UserButton />
+      </Show>
+    </header>
   );
 }
